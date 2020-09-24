d4d07a92bcec0e7b8c700d96f534ff1c3ff50a0e
Shane Duffy <shane@shaneduffy.io>
2020-09-24 15:19:45 -0700

    Bug Fix: was not properly splitting CC strings, and implemented detection for multiple notifications for the same word at the same time

--- js/meet.js
+++ js/meet.js
@@ -1,37 +1,30 @@
 let compareItems = [];
-let currentWordItems = [];
+let currentWordItems = new Map();
 
 var docObserver = new MutationObserver(function (mutations, me) {
-    var divItems = document.getElementsByClassName('CNusmb');
-
+    CCWordFreq = [];
     newWordItems = [];
     itemsChanged = false;
     indexer = 0;
 
+    let divItems = document.getElementsByClassName('CNusmb');
     for (let divItem of divItems) {
-        let content = divItem.innerHTML.replace(/[^\w\s]/gi, '').toLowerCase();
-        
-        for (let contentItem of content.split(' ')) {
-            if (currentWordItems[indexer] === null || currentWordItems[indexer] !== content)
-                itemsChanged = true;
-
-            newWordItems.push(contentItem);
-            indexer++;
+        let CCWords = divItem.innerHTML.replace(/[^\w\s]/gi, '').toLowerCase().split(' ');
+        console.log(CCWords);
+        for (let CCWord of CCWords) {
+            if (CCWordFreq[CCWord])
+                CCWordFreq[CCWord]++;
+            else
+                CCWordFreq[CCWord] = 1;
         }
     }
 
-    if (itemsChanged) {
-        for (let [index, currentCCItem] of currentWordItems.entries()) {
-            if (!newWordItems.includes(currentCCItem)) {
-                currentWordItems.splice(index, 1);
-            }
-        }
+    for (let compareItem of compareItems) {
+        if (CCWordFreq[compareItem]) {
+            if (!currentWordItems[compareItem] || CCWordFreq[compareItem] > currentWordItems[compareItem])
+                chrome.extension.sendMessage({ platform: 'Google Meet', item: compareItem }, function(response) { return true; });
 
-        for (let newCCItem of newWordItems) {
-            if (compareItems.includes(newCCItem) && !currentWordItems.includes(newCCItem)) {
-                chrome.extension.sendMessage({ platform: 'Google Meet', item: newCCItem }, function(response) { });
-                currentWordItems.push(newCCItem);
-            }
+            currentWordItems[compareItem] = CCWordFreq[compareItem];
         }
     }
 });
@@ -47,16 +40,12 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         let resultList = [];
 
-        for (let wordItem of request.WordItems) {
-            if (wordItem.state === '') {
+        for (let wordItem of request.WordItems)
+            if (wordItem.state === '')
                 resultList.push(wordItem.content.toLowerCase());
-            }
-        }
 
         compareItems = resultList;
-
-        sendResponse();
-
+        
         return true;
     });
 
@@ -64,11 +53,9 @@ chrome.storage.sync.get(['WordItems'],
     function(result) {
         let resultList = [];
 
-        for (let wordItem of result.WordItems) {
-            if (wordItem.state === '') {
+        for (let wordItem of result.WordItems)
+            if (wordItem.state === '')
                 resultList.push(wordItem.content);
-            }
-        }
 
         compareItems = resultList;
 
